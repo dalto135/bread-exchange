b71f5dadd8a989fa0717d1a085a5a32a16594b4e
Tre Scott <tres1396@gmail.com>
2021-06-21 14:12:52 -0400

    adding some folders, adding login page js and css, got background to show up

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,11 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Login from "./components/LoginPage/Login"
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Bread Exchange</h2>
-      </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
+    <div>
+      <Login/>
     </div>
   );
 }
---
+++ client/src/components/LoginPage/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css"
+
+function Login() {
+    return (
+        <div className="login-background">
+
+        </div>
+    )
+}
+
+export default Login;
