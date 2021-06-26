7f4c6e04967918d9e1c0b1c995ef14f0775e8dd8
dalto135 <67397294+dalto135@users.noreply.github.com>
2021-06-26 13:18:42 -0400

    fixed routes for heroku

---
+++ models/index.js
@@ -0,0 +1,5 @@
+const User = require('./User');
+const Post = require('./post');
+const Reservation = require('./Reservation');
+
+module.exports = { User, Post, Reservation };
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const apiRoutes = require('./api');
 
-router.use("api",apiRoutes);
+router.use("/api",apiRoutes);
 
 module.exports = router;
 
