3713e1798b25508c08d4ebc6d98d52de84cef6fc
dalto135 <67397294+dalto135@users.noreply.github.com>
2021-06-29 20:32:02 -0400

    updated routes

--- routes/api/post.js
+++ routes/api/post.js
@@ -1,25 +1,45 @@
 const router = require("express").Router();
+const { Post } = require('../../models');
 
-const foodExchangeController = require("../../controller/foodExchangeController.js");
-const Post = require("../../controller/foodExchangeController.js");
+// const foodExchangeController = require("../../controller/foodExchangeController.js");
+// router
+// .route("/")
+// .get(foodExchangeController.findAll)
+// .post(foodExchangeController.create);
 
-router
-.route("/")
-.get(foodExchangeController.findAll)
-.post(foodExchangeController.create);
+//Retrieve information of all posts
+router.get('/', (req, res) => {
+   Post.find({})
+   .then(post => {
+     res.json(post);
+   })
+   .catch(err => {
+     res.status(404).json(err.message);
+   })
+})
 
-// const Post = require("../../controller/foodExchangeController.js")
-//const { Post } = require('../../models') 
+//Make a new post
+router.post('/', (req, res) => {
+    Post.create(req.body)
+    .then(post => {
+      res.json(post);
+    })
+    .catch(err => {
+      res.status(404).json(err.message);
+    })
+ })
 
-//router.get('/', (req, res) => {
-//    Post.find({})
-//    .then(dbBook => {
-//      res.json(dbBook);
-//    })
-//    .catch(err => {
-///      res.status(404).json(err.message);
-//    })
-//})
-
+//Delete post by id
+router.delete('/:id', (req, res) => {
+    Post.remove({_id: req.params.id},
+        function(err, result) {
+            if (err) {
+                res.send(err.message);
+            } else {
+                res.json(result);
+            }
+        }
+    )
+})
 
 module.exports = router;
