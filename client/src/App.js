eaab809a145cfc71f32af76ac82a91d1311250d2
Tre Scott <tres1396@gmail.com>
2021-07-01 19:14:18 -0400

    styling navbar, created some components

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,28 @@
 import React, { Component } from "react";
 import "./App.css";
-import Login from "./pages/LoginPage/Login"
-import Navbar from './components/Navbar/navbar'
-import MainPage from './pages/MainPage/mainpage'
-import Posts from './pages/Posts/Posts';
+import Login from "./pages/LoginPage/Login";
+import Navbar from "./components/Navbar/navbar";
+import MainPage from "./pages/MainPage/mainpage";
+import Posts from "./pages/Posts/Posts";
 import SinglePost from "./pages/Singlepost/Singlepost";
 import CreatePost from "./pages/CreatePost/CreatePost";
 import UserPage from "./pages/UserPage/userpage";
-import { HashRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from "react-router-dom";
 import userPage from "./pages/UserPage/userpage";
+import Button from "./components/Button/Button";
 
 function App() {
   return (
     <Router>
-      <Navbar/>
-      <Route exact path='/' component={MainPage}/>
-      <Route exact path='/login' component={Login}/>
-      <Route exact path='/Posts' component={Posts}/>
-      <Route exact path='/singlepost' component={SinglePost}/>
-      <Route exact path='/CreatePost' component={CreatePost}/>
-      <Route exact path='/UserPage' component={userPage}/>
+      <Navbar />
+      <Route exact path="/" component={MainPage} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/Posts" component={Posts} />
+      <Route exact path="/singlepost" component={SinglePost} />
+      <Route exact path="/CreatePost" component={CreatePost} />
+      <Route exact path="/UserPage" component={userPage} />
     </Router>
   );
 }
-
 
 export default App;
--- client/src/pages/Posts/Posts.js
+++ client/src/pages/Posts/Posts.js
@@ -2,8 +2,8 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import "./style.css";
 import { Link } from "react-router-dom";
-import Card from "react-bootstrap/Card";
-import Button from "react-bootstrap/Button";
+import Input from "../../components/Input/input"
+import Button from "../../components/Button/Button"
 import SinglePost from "../Singlepost/Singlepost";
 import CreatePost from "../CreatePost/CreatePost";
 import { HashRouter as Router, Route } from "react-router-dom";
@@ -11,50 +11,68 @@ import { HashRouter as Router, Route } from "react-router-dom";
 function Posts() {
   return (
     <div>
-      <h1>Find Food Here</h1>
-      <Link to="/CreatePost">Post Food</Link><br></br>
-      <Link to="/singlepost">Food</Link>
-      {/* <Router>
-        <Route exact path='/singlepost' component={SinglePost}/>
-      </Router> */}
-      <div className="card-container">
-        <div className="card">
-          <img className="card-img" src="/chart.jpg" alt="Avatar"></img>
-          <div className="container">
-            <h4>
-              <b>Panera Bread</b>
-            </h4>
-            <h6>Pickup Location Here</h6>
-            <h6>Posted 6/24/21</h6>
-            <h6>Exp: 6/31/21</h6>
-            <h6>Contact: John Smith - jsmith@email.com</h6>
-            <p>
-              <ul>
-                <li>2 loaves of bread</li>
-                <li>3 apples</li>
-              </ul>
-            </p>
-            <button>View</button>
-          </div>
-        </div>
+      {/* Heading */}
+      <div id="find-food-header">
+        <h1>Find Food</h1>
+        <p>
+          Browse listings from our food partners to find foods that convenient
+          for you!{" "}
+        </p>
+        
+      </div>
 
-        <div className="card">
-          <img className="card-img" src="/chart.jpg" alt="Avatar"></img>
-          <div className="container">
-            <h4>
-              <b>Starbucks</b>
-            </h4>
-            <h6>Pickup Location Here</h6>
-            <h6>Posted 6/26/21</h6>
-            <h6>Exp: 6/31/21</h6>
-            <h6>Contact: John Smith - jsmith@email.com</h6>
-            <p>
-              <ul>
-                <li>5 cookies</li>
-                <li>1 gallon milk</li>
-              </ul>
-            </p>
-            <button>View</button>
+
+
+      <Link to="/CreatePost">Post Food</Link>
+      <br></br>
+      <Link to="/singlepost">Food</Link><br></br>
+      <Button title="Post an Item" />
+      <div id="gray-panel">
+        <div id="card-search">
+          <p>Search for an item.</p>
+          {/* Input component */}
+        <Input/>
+        </div>
+        <div className="card-container">
+          {/* Cards */}
+          <div className="card">
+            <img className="card-img" src="/chart.jpg" alt="Avatar"></img>
+            <div className="container">
+              <h4>
+                <b>Panera Bread</b>
+              </h4>
+              <h6>Pickup Location Here</h6>
+              <h6>Posted 6/24/21</h6>
+              <h6>Exp: 6/31/21</h6>
+              <h6>Contact: John Smith - jsmith@email.com</h6>
+              <p>
+                <ul id="card-list">
+                  <li>2 loaves of bread</li>
+                  <li>3 apples</li>
+                </ul>
+              </p>
+              <Button title="View" />
+            </div>
+          </div>
+
+          <div className="card">
+            <img className="card-img" src="/chart.jpg" alt="Avatar"></img>
+            <div className="container">
+              <h4>
+                <b>Starbucks</b>
+              </h4>
+              <h6>Pickup Location Here</h6>
+              <h6>Posted 6/26/21</h6>
+              <h6>Exp: 6/31/21</h6>
+              <h6>Contact: John Smith - jsmith@email.com</h6>
+              <p>
+                <ul>
+                  <li>5 cookies</li>
+                  <li>1 gallon milk</li>
+                </ul>
+              </p>
+              <Button title="View" />
+            </div>
           </div>
         </div>
       </div>
