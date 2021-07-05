114f9f2dc8789ed8cc7b5fc2c554f8816036efb9
Tre Scott <tres1396@gmail.com>
2021-07-05 16:07:54 -0400

    restyling navbar

--- client/src/components/Navbar/navbar.js
+++ client/src/components/Navbar/navbar.js
@@ -6,21 +6,24 @@ function navBar() {
   return (
     <div>
       <div id="navbar">
-        <img id="be-logo" src="/BElogo.jpg"></img>
-        <div id="nav-button-container">
-          <ul id="nav-list">
-            {/* Main Nav Buttons */}
-            <li className="nav-item"><i className="fas fa-home nav-icon"></i><Link id="link-button" to="/">Home</Link></li>
-            <li className="nav-item"><i className="fas fa-sign-in-alt nav-icon"></i><Link id="link-button" to="/login">Login</Link></li>
-            <li className="nav-item"><i className="fas fa-bread-slice nav-icon"></i><Link id="link-button" to="/Posts">Posts</Link></li>
-            <li className="nav-item"><i className="fas fa-user nav-icon"></i><Link id="link-button" to="/UserPage">My Userpage</Link></li>
+        {/* Logo */}
+        <div id="logo-div">
+          <img id="be-logo" src="/BElogo.jpg"></img>
+        </div>
+        {/* Buttons */}
+        <div id="buttons-div">
+          <ul id="nav-buttons-list">
+            <li className="list-item"><i className="fas fa-home nav-icon"></i><Link className="link-button" to="/">Home</Link></li>
+            <li className="list-item"><i className="fas fa-sign-in-alt nav-icon"></i><Link className="link-button" to="/login">Login</Link></li>
+            <li className="list-item"><i className="fas fa-bread-slice nav-icon"></i><Link className="link-button" to="/Posts">Posts</Link></li>
+            <li className="list-item"><i className="fas fa-user nav-icon"></i><Link className="link-button" to="/UserPage">My Userpage</Link></li>
           </ul>
         </div>
-        {/* Cart and Logout Buttons */}
-        <div id="cart-and-logout">
-          <ul id="nav-list">
-            <li className="nav-item"><i className="fas fa-shopping-cart nav-icon"></i><Link id="link-button" to="/"></Link>Reservations</li>
-            <li className="nav-item"><Link id="link-button" to="/">Logout</Link></li>
+        {/* Cart and Logout */}
+        <div id="cart-and-logout-div">
+          <ul id="nav-buttons-list">
+            <li className="list-item"><i className="fas fa-shopping-cart nav-icon"></i><Link className="link-button" to="/">Reservations</Link></li>
+            <li className="list-item"><Link className="link-button" to="/">Logout</Link></li>
           </ul>
         </div>
       </div>
