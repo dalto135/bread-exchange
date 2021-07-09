e1a77ec930a497e9370e043da59767f79a290907
dalto135 <67397294+dalto135@users.noreply.github.com>
2021-07-08 21:49:22 -0400

    object

--- models/index.js
+++ models/index.js
@@ -1,5 +1,11 @@
-const User = require('./user');
-const Post = require('./post');
-const Reservation = require('./reservation');
+// const User = require('./user');
+// const Post = require('./post');
+// const Reservation = require('./reservation');
 
-module.exports = { User, Post, Reservation };
+// module.exports = { User, Post, Reservation };
+
+module.exports = {
+    User: require('./user'),
+    Post: require('./post'),
+    Reservation: require('./reservation')
+};
